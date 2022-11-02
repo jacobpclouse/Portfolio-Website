21a6aa0ad0b1f922f5cc3ff507fc3f8ba8cd42b3
jacobpclouse <clouse.jp@gmail.com>
2022-11-02 12:31:57 -0400

    linked styles and created index page, will work on extending pages, think i just need one portfolio base page and can fill out data from a central database

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-# IMPORT STATEMENTS
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# IMPORTS
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # FUNCTIONS
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # ROUTES
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# @app.route('/')
+# def myBasePage():
+#     title = "Jacob Clouse Portfolio Website"
+#     return render_template('base.html',html_title = title)
+
+    # Index page
 @app.route('/')
 def myIndexPage():
     title = "Jacob Clouse Portfolio Website"
-    return render_template('base.html',html_title = title)
+    return render_template('index.html',html_title = title)
 
 
 # main statement - used to set dev mode
